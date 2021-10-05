4152c50cd2c951d763f094e93c5f5e21871de680
dk <s7plc@ya.ru>
2021-10-05 14:03:27 +0300

    udt_try read, not success(

--- udt_read.py
+++ udt_read.py
@@ -1,12 +1,10 @@
 from pycomm3 import LogixDriver
 
 
-def read_udt():
+def read_udt(tag):
     with LogixDriver('192.168.32.39/1') as plc:
-        # return plc.read('HMI_IND')
-        return plc.get_tag_list(program='*')
-
-print(read_udt())
+        return plc.read(tag)
+        #return plc.get_tag_list(program='*')
 
 
-
+print(read_udt('HMI_IND'))
