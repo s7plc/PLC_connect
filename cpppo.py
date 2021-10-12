fbb291a46e0767ad3430bb48482f850366c10ebc
dk <s7plc@ya.ru>
2021-10-12 14:18:31 +0300

    cpppo tested

--- cpppo.py
+++ cpppo.py
@@ -1,6 +1,10 @@
 from cpppo.server.enip import client
 
-
-HOST = "192.168.1.100"
+# HOST = "192.168.32.68" it works(..)
+HOST = "10.10.25.100"
 TAGS = ["@4/100/3"]
+
+with client.connector(host=HOST) as conn:
+    operations=client.parse_operations(TAGS)
+print(operations)
 
