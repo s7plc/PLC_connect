fbfc48658f4e85f8e226abbeab2ea7fac72c64d0
dk <s7plc@ya.ru>
2021-12-27 14:29:03 +0300

    cpppo tested

--- cpppo.py
+++ cpppo.py
@@ -1,7 +1,7 @@
 from cpppo.server.enip import client
 
 # HOST = "192.168.32.68" banbury deprag(..)
-HOST = "10.10.25.100"
+# HOST = "10.10.25.100"
 TAGS = ["@4/100/3"]
 
 with client.connector(host=HOST) as conn:
