1d4ee7a2643072184411ac9225a5e0ff4cefe896
dmitry.kuznetsov <ppp>
2022-04-07 20:40:57 +0300

    cabin.py inserted

--- cabins.py
+++ cabins.py
@@ -1,4 +1,12 @@
 n = int(input('Enter cabins amount\n'))
 x = int(input('Enter cabin number\n'))
 y = (n - x) - x + 1
-print(y)
+if n < x:
+    print('you are stupid')
+elif y == 0:
+    print("You reach your cabin")
+elif y < 0:
+    print(f"go backward {y * -1} cabins")
+else:
+    print(f'go forward {y} cabins')
+
