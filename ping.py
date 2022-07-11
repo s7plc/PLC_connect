b38da4b476f0fc32fdda1fe27369ae0abbe25d75
dmitry.kuznetsov <s7plc@ya.ru>
2022-07-11 12:01:47 +0300

    ip changed for lab

--- ping.py
+++ ping.py
@@ -1,8 +1,9 @@
 import os
 
 try:
-    hostname = '192.168.32.39'
-    ping = 4
-    response = os.system(f'ping {hostname} -c{ping}')
+    hostname = '172.31.38.130'
+    #ping = 4
+    #response = os.system(f'ping {hostname} -c{ping}')
+    response = os.system(f'ping {hostname} ')
 except Exception as ex:
     print(ex)
